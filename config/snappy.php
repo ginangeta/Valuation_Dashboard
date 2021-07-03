cabd3c2407c8885a13e7bb191c0909bd39be9dd1
ginangeta <ginangeta@gmail.com>
2021-07-03 13:36:23 +0300

    Gina:Checking out changes

--- config/snappy.php
+++ config/snappy.php
@@ -35,7 +35,7 @@ return [
     
     'pdf' => [
         'enabled' => true,
-        'binary' => base_path('vendor/wemersonjanuario/wkhtmltopdf-windows/bin/64bit/wkhtmltopdf'),
+        'binary' => base_path('vendor/wemersonjanuario/wkhtmltopdf-windows/bin/wkhtmltopdf-windows'),
         'timeout' => false,
         'options' => [],
         'env'     => [],
