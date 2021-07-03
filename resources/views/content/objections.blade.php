d120ac3200807cdd85d93903c6d0f4323f6e8c09
ginangeta <ginangeta@gmail.com>
2021-07-03 20:55:22 +0300

    Gina:Checking out changes

--- resources/views/content/objections.blade.php
+++ resources/views/content/objections.blade.php
@@ -18,7 +18,7 @@
                                 class="btn btn-warning btn-print-docs text-white">Print Page
                                 Objections</a>
                         @else
-                            <a href="printNextObjections/{{ str_replace('/', '-', $ObjectionsUrl) }}"
+                            <a href="{{ route('printNextObjections/') }}{{ strtolower(str_replace('/', '-', $ObjectionsUrl)) }}"
                                 class="btn btn-warning btn-print-docs text-white">Print Page
                                 Objections</a>
                         @endif
